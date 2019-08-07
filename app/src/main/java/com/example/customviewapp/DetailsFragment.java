c7a06716e7776a0801c04c85287aaa22846eafdb
abdul <abdul.tanveer@gmail.com>
2019-08-07 19:38:01 +0530

    details fragment was added

---
+++ app/src/main/java/com/example/customviewapp/DetailsFragment.java
@@ -0,0 +1,21 @@
+package com.example.customviewapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class DetailsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+         super.onCreateView(inflater, container, savedInstanceState);
+         View layout_inflated = inflater.inflate(R.layout.details_fragment,container,false);
+         return layout_inflated;
+    }
+}
